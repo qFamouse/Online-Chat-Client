bb51f9b0c3fbbad37c87e80780e0b7659c00fdb0
Famouse <famouse_ru@mail.ru>
2023-01-06 22:20:02 +0300

    chore: install angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { AuthComponent } from "./pages/auth/auth.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
 	declarations: [AppComponent, AuthComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		MatFormFieldModule,
+		MatInputModule,
+		ReactiveFormsModule,
+		MatButtonModule
+	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
