3363d9c9d812e687dbc73ba5dbb52319359af149
Famouse <famouse_ru@mail.ru>
2023-01-07 21:54:40 +0300

    chore: create empty user service

---
+++ src/app/shared/modules/api/services/user.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+import { ApiService } from "../../../services/api.service";
+
+@Injectable({
+	providedIn: "root"
+})
+export class UserService {
+	constructor(private apiService: ApiService) {}
+}
