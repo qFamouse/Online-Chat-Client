7c29705bbb6ac02d0adbcbafcb632d1bdff986d7
Famouse <famouse_ru@mail.ru>
2023-01-07 21:58:38 +0300

    chore: create component for direct messages, dialogs html coding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { RouterModule, Routes } from "@angular/router";
 import { AuthComponent } from "./pages/auth/auth.component";
 import { NgModule } from "@angular/core";
+import { DirectComponent } from "./pages/chat/components/direct/direct.component";
 
 const routes: Routes = [
 	{
 		path: "",
 		component: AuthComponent
+	},
+	{
+		path: "direct",
+		component: DirectComponent
 	}
 ];
 
---
+++ src/app/pages/chat/components/direct/direct.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-direct',
+  templateUrl: './direct.component.html',
+  styleUrls: ['./direct.component.scss']
+})
+export class DirectComponent {
+
+}
