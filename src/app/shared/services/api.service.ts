0a52d18159af87a99c629f945148cc85ec8f0848
Famouse <famouse_ru@mail.ru>
2023-01-07 21:53:27 +0300

    chore: complete api service and create shared module

---
+++ src/app/shared/services/api.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+	providedIn: "root"
+})
+export class ApiService {
+	constructor(private http: HttpClient) {}
+
+	get<T>(path: string, params: HttpParams = new HttpParams()): Observable<T> {
+		return this.http.get<T>(`${path}`, { params });
+	}
+
+	post<T>(path: string, body: Object = {}): Observable<T> {
+		return this.http.post<T>(path, body);
+	}
+
+	put<T>(path: string, body: Object = {}): Observable<T> {
+		return this.http.put<T>(path, body);
+	}
+
+	patch<T>(path: string, body: Object = {}): Observable<T> {
+		return this.http.patch<T>(path, body);
+	}
+
+	delete(path: string): Observable<boolean> {
+		return this.http.delete<boolean>(path);
+	}
+}
