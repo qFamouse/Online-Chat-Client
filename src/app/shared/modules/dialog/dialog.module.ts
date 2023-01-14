e14de24be4ee6ef5d5a341b4d59dc99c52515009
Famouse <famouse_ru@mail.ru>
2023-01-14 21:37:30 +0300

    chore: create module for dialogs

---
+++ src/app/shared/modules/dialog/dialog.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ErrorDialogComponent } from "./components/error-dialog/error-dialog.component";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+
+@NgModule({
+	declarations: [ErrorDialogComponent],
+	imports: [CommonModule, MatDialogModule, MatButtonModule, MatIconModule]
+})
+export class DialogModule {}
