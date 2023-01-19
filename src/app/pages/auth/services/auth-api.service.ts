433b23c4fabf8ebebe162d231172093ef85fb992
Famouse <famouse_ru@mail.ru>
2023-01-19 20:36:13 +0300

    fix: replacing tap with switchMap (for sequential requests (synchronize))

--- src/app/pages/auth/services/auth-api.service.ts
+++ src/app/pages/auth/services/auth-api.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from "@angular/core";
 import { JwtAuthService } from "../../../shared/services/jwt-auth.service";
 import { SignupForm } from "../models/signup-form.model";
-import { Observable, tap } from "rxjs";
-import { User } from "../../../shared/models/entities/user.entity";
+import { Observable, switchMap } from "rxjs";
 import { LoginForm } from "../models/login-form.model";
 import { JwtSession } from "../../../shared/models/dto/jwt-session.dto";
 
@@ -12,14 +11,11 @@ import { JwtSession } from "../../../shared/models/dto/jwt-session.dto";
 export class AuthApiService {
 	constructor(private jwtAuthService: JwtAuthService) {}
 
-	signup(form: SignupForm): Observable<User> {
+	signup(form: SignupForm): Observable<JwtSession> {
 		return this.jwtAuthService
 			.signup(form.userName, form.email, form.password)
 			.pipe(
-				tap(user => this.jwtAuthService.login(form.email, form.password))
-				// for avatar create second request, and use switchMap (for synchronize)
-				//switchMap(() => this.jwtAuthService.login(form.email, form.password))
-				//switchMap(updateImage)
+				switchMap(() => this.jwtAuthService.login(form.email, form.password))
 			);
 	}
 
