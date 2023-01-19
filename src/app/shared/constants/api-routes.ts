b8132c35f502c141b3890fdcfc6b8d48f45a9e44
Famouse <famouse_ru@mail.ru>
2023-01-19 17:35:52 +0300

    chore: adding authenticate and about api routes

--- src/app/shared/constants/api-routes.ts
+++ src/app/shared/constants/api-routes.ts
@@ -4,5 +4,7 @@ const apiUrl = environment.apiUrl;
 
 export const apiUserRoutes = {
 	login: `${apiUrl}/Users/login`,
-	signup: `${apiUrl}/Users/register`
+	signup: `${apiUrl}/Users/register`,
+	authenticate: `${apiUrl}/Users/auth`,
+	about: `${apiUrl}/Users/about`
 };
