f04b30dfacc437a187ffee4702319551221196e8
Famouse <famouse_ru@mail.ru>
2023-01-23 22:41:42 +0300

    chore: adding method to get user

--- src/app/shared/modules/api/services/user.service.ts
+++ src/app/shared/modules/api/services/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from "@angular/core";
 import { ApiService } from "../../../services/api.service";
+import { Observable } from "rxjs";
+import { apiUserRoutes } from "../../../constants/api-routes";
+import { User } from "../../../models/entities/user.entity";
 
 @Injectable({
 	providedIn: "root"
 })
 export class UserService {
 	constructor(private apiService: ApiService) {}
+
+	getUserById(id: number): Observable<User> {
+		let url = `${apiUserRoutes.user}/${id}`;
+		return this.apiService.get(url);
+	}
 }
