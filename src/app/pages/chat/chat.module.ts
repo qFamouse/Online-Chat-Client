59865310c133cd05949d291d9614b7b11b708a59
Famouse <famouse_ru@mail.ru>
2023-01-23 22:46:43 +0300

    chore: declare new component

--- src/app/pages/chat/chat.module.ts
+++ src/app/pages/chat/chat.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import { DirectComponent } from "./components/direct/direct.component";
 import { AutosizeModule } from "ngx-autosize";
-import { MessageComponent } from './components/message/message.component';
-import { DialogComponent } from './components/dialog/dialog.component';
+import { MessageComponent } from "./components/message/message.component";
+import { DialogComponent } from "./components/dialog/dialog.component";
+import { ConversationComponent } from "./components/conversation/conversation.component";
 
 @NgModule({
 	imports: [CommonModule, RouterOutlet, AutosizeModule],
-	declarations: [DirectComponent, MessageComponent, DialogComponent]
+	declarations: [
+		DirectComponent,
+		MessageComponent,
+		DialogComponent,
+		ConversationComponent
+	]
 })
 export class ChatModule {}
