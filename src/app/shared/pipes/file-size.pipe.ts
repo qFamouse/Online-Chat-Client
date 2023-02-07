046d42cd6f35af08d2054a3ad4ce14ded3fd8adc
Famouse <famouse_ru@mail.ru>
2023-02-07 20:25:52 +0300

    chore: units name same for everyone

--- src/app/shared/pipes/file-size.pipe.ts
+++ src/app/shared/pipes/file-size.pipe.ts
@@ -22,9 +22,8 @@ export class FileSizePipe implements PipeTransform {
 			return bytes + " B";
 		}
 
-		const units = si
-			? ["KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
-			: ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+		const units = ["KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+		// const units = si	? ["KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"] : ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
 		let u = -1;
 		const r = 10 ** dp;
 
