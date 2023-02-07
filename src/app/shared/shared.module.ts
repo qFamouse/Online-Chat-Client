bbead9622097038d45e5301f3b18b40daa88aa1f
Famouse <famouse_ru@mail.ru>
2023-02-07 19:03:19 +0300

    chore: adding file size pipe (transform bytes to human readable string)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ApiService } from "./services/api.service";
+import { FileSizePipe } from "./pipes/file-size.pipe";
 
 @NgModule({
-	declarations: [],
+	declarations: [FileSizePipe],
 	imports: [CommonModule],
+	exports: [FileSizePipe],
 	providers: [ApiService]
 })
 export class SharedModule {}
